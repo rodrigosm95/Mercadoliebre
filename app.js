d868a8f22d13c1aa0a89b0e9296be89e7de659e2
rodrigosm95 <rodrigo.sanmartin95@gmail.com>
2022-01-05 11:00:01 -0300

    añado archivo procfile

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const app = express();
 
 app.use(express.static(publicPath));
 
-app.set('port', process.env.PORT || 3000)
-
-app.listen(app.get('port'), () => console.log("levantando servidor en el puerto" + app.get('port')));
+app.listen(process.env.PORT || 3000, ()=> console.log("levantando servidor en el puerto 3000") )
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve("./views/home.html"))
